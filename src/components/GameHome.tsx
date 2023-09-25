e39ec36ec0057cba2e9f050c320966b058379fd4
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-09-25 20:59:36 +0900

    homeの背景画像を設定

--- src/components/GameHome.tsx
+++ src/components/GameHome.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './GameHome.scss';
-import newButton from '../assets/images/atarasii.gif';
 
 const GameHome = () => {
   return (
-    <div className="AppHome">
-			<h1 className="text-4xl font-bold leading-tight py-5">黄泉</h1>
-			<p className="text-2xl">-地獄からの誘い-</p>
-			<img src={newButton} alt="new game" />
-		</div>
+    <div className="AppHome min-h-screen flex flex-col justify-between">
+      <div className='p-5'>
+        <h1 className="text-4xl font-bold leading-tight py-3">黄泉</h1>
+        <p className="text-2xl">-地獄からの誘い-</p>
+      </div>
+      <div className='flex-grow flex items-center justify-center'>
+        <div className='p-5 shadow-lg rounded'>
+          <p className="text-2xl mb-2">はじめる</p>
+          <p className="text-2xl">設定</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default GameHome
+export default GameHome;
