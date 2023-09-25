a70f11ce00384e48ecd08fdd1e2fcfeed4146e3e
KenTanaka <k.tanaka@apptime.co.jp>
2023-09-25 16:49:43 +0900

    gamehomeコンポーネントの作成

---
+++ src/components/GameHome.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GameHome.scss';
+
+const GameHome = () => {
+  return (
+    <div className="AppHome">
+		<h1>黄泉</h1>
+		<p className="text-blue-500 text-2xl my-4">tailwindが当たっているか？</p>
+		</div>
+  )
+}
+
+export default GameHome
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
