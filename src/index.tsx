cf71a32c730f12612da22ac3abe45ac68770bff8
KenTanaka <k.tanaka@apptime.co.jp>
2023-09-26 13:02:08 +0900

    phaserを追加
    ゲーム用のjsライブラリ

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './index.scss';
-import GameHome from './components/GameHome';
+import HomeScene from './components/HomeScene';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* home */}
-    <GameHome />
+    <HomeScene />
   </React.StrictMode>
 );
